0ec467396cf4451e59bdaadf984e2e4da83d1d78
Muhammad Ibrahim <m.ibrahim.work32@gmail.com>
2026-09-03 16:01:44 +0500

    initial commit

--- src/main/java/com/Encryptiondemo/encryptiondecryption/demo/services/decryption.java
+++ src/main/java/com/Encryptiondemo/encryptiondecryption/demo/services/decryption.java
@@ -21,37 +21,20 @@ public class decryption {
 
     public String decryptdata() throws Exception {
 
-        // get the same AES key
         SecretKey key = keyGenerator.getKey();
-
-        // create AES cipher
         Cipher cipher = Cipher.getInstance("AES");
-
-        // tell cipher that we want to decrypt
         cipher.init(Cipher.DECRYPT_MODE, key);
-
-        // read Base64 encrypted text from file
         String encryptedData = readData.getencrypted();
-
-        // convert Base64 text back into encrypted bytes
         byte[] encryptedBytes =
                 Base64.getDecoder().decode(encryptedData);
-
-        // decrypt the encrypted bytes
         byte[] decryptedBytes =
                 cipher.doFinal(encryptedBytes);
-
-        // convert decrypted bytes back into normal text
         String decryptedData =
                 new String(decryptedBytes);
-
-        // save decrypted data into a different file
         Files.writeString(
                 Path.of("C:\\Users\\Public\\Documents\\INTERNSHP PROJECTS INTELIJ\\encryptiondecryption-demo\\Decrypteddata.txt"),
                 decryptedData
         );
-
-        // return original data
         return decryptedData;
     }
 }
